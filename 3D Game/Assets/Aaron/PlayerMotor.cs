298e3864f6e077004f5ea37a0414cf2b23eaeacc
SwordOfWrynn <bubba.wiens@Gmail.com>
2018-04-10 14:00:52 -0700

    foo

--- 3D Game/Assets/Aaron/PlayerMotor.cs
+++ 3D Game/Assets/Aaron/PlayerMotor.cs
@@ -3,9 +3,34 @@
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMotor : MonoBehaviour
 {
+    private Vector3 velocity = Vector3.zero;
 
+    private Rigidbody rb;
 
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
+    //Gets a movement vector
+    public void Move(Vector3 controlVelocity)
+    {
+        velocity = controlVelocity;
+    }
 
+    //Run every physics iteration
+    void FixedUpdate()
+    {
+        PerformMovement();
+    }
+
+    //Perform movement based on velocity variable
+    void PerformMovement()
+    {
+        if (velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+    }
 
 }
