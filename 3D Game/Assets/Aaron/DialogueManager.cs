c83f7363bd3a7d4d4f61cc4daf89d4a8e30fd57d
SwordOfWrynn <bubba.wiens@Gmail.com>
2018-04-12 13:59:39 -0700

    Dialogue boxes

---
+++ 3D Game/Assets/Aaron/DialogueManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour {
+
+    private Queue<string> sentences;
+
+	// Use this for initialization
+	void Start () {
+        sentences = new Queue<string>();
+	}
+    public void StartDialogue(Dialogue dialogue)
+    {
+        Debug.Log("Starting dialogue with/about" + dialogue.name);
+    }
+}
