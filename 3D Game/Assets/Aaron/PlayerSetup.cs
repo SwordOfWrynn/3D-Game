d030563dc99f1060353afae52a7635288a738a2d
SwordOfWrynn <bubba.wiens@Gmail.com>
2018-04-12 12:44:28 -0700

    Scripting Work

---
+++ 3D Game/Assets/Aaron/PlayerSetup.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour {
+
+    [SerializeField]
+    Behaviour[] componentsToDisable;
+
+    private Camera sceneCamera;
+
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            for (int i = 0; i < componentsToDisable.Length; i++)
+                componentsToDisable[i].enabled = false;
+        }
+        else
+        {
+            sceneCamera = Camera.main;
+            if (sceneCamera != null)
+                sceneCamera.gameObject.SetActive(false);
+        }
+    }
+
+    void OnDisable()
+    {
+        if (sceneCamera != null)
+            sceneCamera.gameObject.SetActive(true);
+    }
+
+}
