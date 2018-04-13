ff26cb09b47b60f1c1ee3eee1ebc4f8889cd46fa
SwordOfWrynn <bubba.wiens@Gmail.com>
2018-04-13 12:46:38 -0700

    Added Dialogue Box prefab

--- 3D Game/Assets/Aaron/DialogueManager.cs
+++ 3D Game/Assets/Aaron/DialogueManager.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour {
 
+    public Text nameText;
+    public Text dialogueText;
+
+    public Animator anim;
+    
     private Queue<string> sentences;
 
 	// Use this for initialization
@@ -12,6 +18,45 @@ public class DialogueManager : MonoBehaviour {
 	}
     public void StartDialogue(Dialogue dialogue)
     {
-        Debug.Log("Starting dialogue with/about" + dialogue.name);
+        anim.SetBool("isOpen", true);
+
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach(string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence (string sentence)
+    {
+        dialogueText.text = "";
+        foreach(char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    void EndDialogue()
+    {
+        anim.SetBool("isOpen", false);
+        Debug.Log("End of dialogue");
     }
 }
