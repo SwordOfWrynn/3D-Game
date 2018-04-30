3e5e4eb2c71553246b29171f2358fa6cfef0f157
SwordOfWrynn <bubba.wiens@gmail.com>
2018-04-29 17:32:57 -0700

    Moved some of my stuff around

---
+++ 3D Game/Assets/Aaron/Network Testing/Scripts/PauseMenu.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    public static bool isOn = false;
+
+}
---
+++ 3D Game/Assets/Aaron/Network Testing/Scripts/PlayerUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerUI : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject pauseMenu;
+
+	// Use this for initialization
+	void Start () {
+        PauseMenu.isOn = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePauseMenu();
+        }
+	}
+
+    void TogglePauseMenu()
+    {
+        pauseMenu.SetActive(!pauseMenu.activeSelf);
+        PauseMenu.isOn = pauseMenu.activeSelf;
+    }
+}
