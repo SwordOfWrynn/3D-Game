3738ec5d016497406a6b1bd34fdebfda2e6aa2f8
SwordOfWrynn <bubba.wiens@Gmail.com>
2018-05-01 13:33:50 -0700

    Foo

---
+++ 3D Game/Assets/Aaron/NextLevel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NextLevel : MonoBehaviour {
+
+    public string nextLevelName;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void LoadNextLevel()
+    {
+        SceneManager.LoadScene(nextLevelName);
+    }
+
+}
