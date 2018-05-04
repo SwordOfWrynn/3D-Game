ffc25fd66ea0773445afdf22001004d897927644
SwordOfWrynn <bubba.wiens@Gmail.com>
2018-05-04 11:52:23 -0700

    Foo

--- 3D Game/Assets/Aaron/MainMenu.cs
+++ 3D Game/Assets/Aaron/MainMenu.cs
@@ -5,22 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
     public void GoToLobby()
     {
         SceneManager.LoadScene("Lobby");
     }
 
     public void GoToAchievments()
+    {
+        SceneManager.LoadScene("Achievments");
+    }
+
+
+    public void GoToMainMenu()
     {
         SceneManager.LoadScene("MainMenu");
     }
