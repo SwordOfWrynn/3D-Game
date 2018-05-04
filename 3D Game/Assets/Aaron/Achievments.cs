7f1ded2a0ec289547bb3a28a7dfff816d939fce7
SwordOfWrynn <bubba.wiens@gmail.com>
2018-05-03 18:56:04 -0700

    Added achievments scene and script

---
+++ 3D Game/Assets/Aaron/Achievments.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class Achievments : MonoBehaviour {
+
+    public Text achiev1;
+    public Text achiev2;
+    public Text achiev3;
+    public Text achiev4;
+
+	// Use this for initialization
+	void Start () {
+        if (File.Exists(Application.persistentDataPath + "/Achievments.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/Achievments.dat", FileMode.Open);
+            SaveLoad.AchievInfo myLoadedInfo = (SaveLoad.AchievInfo)bf.Deserialize(file);
+            achiev1.text = myLoadedInfo.achiev1;
+            achiev2.text = myLoadedInfo.achiev2;
+            achiev3.text = myLoadedInfo.achiev3;
+            achiev4.text = myLoadedInfo.achiev4;
+        }
+    }
+	
+}
--- 3D Game/Assets/Aaron/SaveLoad.cs
+++ 3D Game/Assets/Aaron/SaveLoad.cs
@@ -18,7 +18,7 @@ public class SaveLoad : MonoBehaviour
         BinaryFormatter bf = new BinaryFormatter();
         FileStream file = File.Open(Application.persistentDataPath + "/Achievments.dat", FileMode.OpenOrCreate);
         AchievInfo myInfo = new AchievInfo();
-        //for second achiev set myInfo.achiev2 = to the achiev, for third achiev3, etc.
+        //for second achiev set myInfo.achiev2 = to the achiev, for third achiev3, and fourth is achiev4.
         myInfo.achiev1 = achiev;
         bf.Serialize(file, myInfo);
         file.Close();
@@ -38,5 +38,8 @@ public class SaveLoad : MonoBehaviour
     {
         // add a achiev for each avaliable achiev
         public string achiev1;
+        public string achiev2;
+        public string achiev3;
+        public string achiev4;
     }
 }
