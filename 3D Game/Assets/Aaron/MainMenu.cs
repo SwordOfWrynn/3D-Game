27c250d8684da3f23b134a7586263bb8d3b7ad09
Aliya Pangelinan <18.aliya.pangelinan@ksd.org>
2018-05-23 12:41:00 -0700

    fix menu

--- 3D Game/Assets/Aaron/MainMenu.cs
+++ 3D Game/Assets/Aaron/MainMenu.cs
@@ -22,9 +22,9 @@ public class MainMenu : MonoBehaviour {
             Debug.LogError("I don't know what's wrong");
 		Cursor.lockState = CursorLockMode.None;
         mainMenuEmpty.SetActive(true);
-		lobby2.interactable = false;
-		lobby3.interactable = false;
-		lobby4.interactable = false;
+		lobby2.interactable = true;
+		lobby3.interactable = true;
+		lobby4.interactable = true;
         if (levels >= 1)
           lobby2.interactable = true;
         if (levels >= 2)
