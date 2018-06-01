39148dc612e4b0e66a5e4608eadab418d41f3bba
Aliya Pangelinan <18.aliya.pangelinan@ksd.org>
2018-06-01 12:57:03 -0700

    shoot script

---
+++ 3D Game/Assets/Scripts/GunScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class GunScript : MonoBehaviour {
+
+	public float damage = 10f;
+	public float range = 100f;
+	
+	public camera fpsCam;
+
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetButtonDown ("Fire1")) 
+		{
+			Shoot ();
+		}
+	
+	}
+
+	void Shoot()
+	{
+		RaycastHit hit;
+		if (Physics.Raycast (fpsCam.transform.position, fpsCam.trasform.forward, out hit, range)) 
+		{
+			Debug.Log (hit.transform.name);
+		}
+	}
+}
