d6199e5344829180a85879fd7da962793d9ae478
Aliya Pangelinan <18.aliya.pangelinan@ksd.org>
2018-06-01 13:44:38 -0700

    attempt to use gun script

--- 3D Game/Assets/Scripts/GunScript.cs
+++ 3D Game/Assets/Scripts/GunScript.cs
@@ -5,24 +5,24 @@ public class GunScript : MonoBehaviour {
 	public float damage = 10f;
 	public float range = 100f;
 	
-	public camera fpsCam;
+	public Camera fpsCam;
 
 	// Update is called once per frame
 	void Update () {
 	
 		if (Input.GetButtonDown ("Fire1")) 
 		{
-			Shoot ();
+			Shoot();
 		}
 	
 	}
 
-	void Shoot()
+	void Shoot ()
 	{
 		RaycastHit hit;
-		if (Physics.Raycast (fpsCam.transform.position, fpsCam.trasform.forward, out hit, range)) 
+		if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range)) 
 		{
-			Debug.Log (hit.transform.name);
+			Debug.Log(hit.transform.name);
 		}
 	}
 }
