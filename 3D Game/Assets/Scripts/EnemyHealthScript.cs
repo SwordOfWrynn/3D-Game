249cf3780a96ac197a884aa13f9f84c9e0122b43
William Burton <18.william.burton@ksd.org>
2018-06-04 13:25:31 -0700

    do not crash on us again

---
+++ 3D Game/Assets/Scripts/EnemyHealthScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealthScript : MonoBehaviour {
+
+    public int EnemyMaxHealth;
+    public int CurrentHealth;
+
+	// Use this for initialization
+	void Start () {
+        CurrentHealth = EnemyMaxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(CurrentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+		
+	}
+    //public void HurtEnemy
+
+
+
+}
